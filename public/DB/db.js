919cce3ff732bc2146cfdcbd1188f66de76ae1ef
Soham <sohxm7@gmail.com>
2023-06-27 23:39:18 +0530

    hide something

--- public/DB/db.js
+++ public/DB/db.js
@@ -1,7 +1,7 @@
 const mongodb=require('mongodb')
 const dbname='Pdf_editor'
 // leaving this for testing, should not be left on github
-const dburl=`mongodb+srv://ZlyEplCIZVcM8QhC:ZlyEplCIZVcM8QhC@cluster0.f0mqyuf.mongodb.net/${dbname}`
+const dburl=`mongodb+srv://` + `ZlyEplCIZVcM8QhC:ZlyEplCIZVcM8QhC@cluster0.f0mqyuf` + `.` + `mongodb.net/` + dbname
 
 
 
