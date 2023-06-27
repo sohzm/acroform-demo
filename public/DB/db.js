a106e78626712afa547bc401294aa39be8d2cda3
Soham <sohxm7@gmail.com>
2023-06-28 03:40:16 +0530

    working on large files

--- public/DB/db.js
+++ public/DB/db.js
@@ -1,8 +1,11 @@
 const mongodb=require('mongodb')
 const dbname='Pdf_editor'
 // leaving this for testing, should not be left on github
-const dburl=`mongodb+srv://` + `ZlyEplCIZVcM8QhC:ZlyEplCIZVcM8QhC@cluster0.f0mqyuf` + `.` + `mongodb.net/` + dbname
+//const dburl=`mongodb+srv://ZlyEplCIZVcM8QhC:ZlyEplCIZVcM8QhC@cluster0.f0mqyuf.mongodb.net/${dbname}`
 
+
+var dbpass = 'iO6gjS7hInAZlnvg'
+const dburl = `mongodb+srv://sohxm7:${dbpass}@clusterx.6wa1yjf.mongodb.net/${dbname}`;
 
 
 module.exports={mongodb,dbname,dburl}
